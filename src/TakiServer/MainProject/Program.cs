71d39e88a2c5ef3baa5ff2d53a1a0c50d45e5a00
yota0808 <yotamlimor@gmail.com>
2024-12-17 18:21:15 +0200

    Create VS projects

---
+++ src/TakiServer/MainProject/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MainProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+			Console.Write("End of program. Any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
